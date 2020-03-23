1fdd356b43ce5092d74e5deccc75d52ceace66d9
Bob J <bobjas777@hotmail.com>
2020-03-23 14:45:06 -0700

    created metods to server all endponts to create  stores and items. Application doesn't save any  data

--- app2.py
+++ app2.py
@@ -1,30 +1,64 @@
 #simple flask application with some endpoints 
-from  flask import Flask
+from  flask import Flask,jsonify,request
 app=Flask(__name__)
 stores=[
     {'name':'My own private store',
-    'items':[
-        {'name':'Toilet Paper 18 pack','price':9.0 }
-    ]
+    'items':
+        [
+        {'name':'Toilet Paper 18 pack',
+        'price':9.0 
+        }
+        ]
+    }
 ]
 @app.route('/store',methods=['POST'])
 def create_store():
-    pass 
+    request_data=request.get_json() 
+    new_store={
+        'name':request_data['name'],
+        'items':[]
+
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 # get store</<string:name>
 @app.route('/store/<string:name>')
 def get_store(name):
-    pass 
+    for store in stores:
+        if store['name']==name:
+            return jsonify(store)
+        else: 
+            return jsonify({'message':'Store not found'}) 
 
-@app.route('store')
+@app.route('/store')
 def get_stores():
-    pass
+    return jsonify({'stores':stores})
 
 @app.route('/store/<string:name>/item',methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data=request.get_json()
+    for store in stores:
+        if store['name']=='name':
+            new_item={
+                'name':request_data['name'],
+                'price':request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message':'Store not found'})
+
+
+    
+
+
 @app.route('/store/<string:name>/item')
 def get_item_in_store(name):
-    pass
+    for store in stores: 
+        if store['name']==name:
+            return jsonify(store['items'])
+        else: 
+            return jsonify({'message':'Store not found'}) 
+
 
 
 
