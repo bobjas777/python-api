5548d6e32e293ff624c500c7f0f0f6768a84228a
Bob J <bobjas777@hotmail.com>
2020-03-22 21:38:38 -0700

    refactored class bookshelf so it can  add as many books ad class book is no longer inherited  from bookshelf. Applied composition

--- class_composition.py
+++ class_composition.py
@@ -4,10 +4,15 @@ class Bookshelf:
     def __str__(self):
         return f"Bookshel with {len(self.books)} books "
 class Book:
-    def __init__(self,nameq,uantity):
+    def __init__(self,name):
+        self.nane=name
+
+    def __str__(self):
+        return  f"Book {self.name}"
 
 
-shelf=Bookshelf(300)
+book1=Book("Harry Potter")
+book2=Book("Bob The winner")
+shelf=Bookshelf(book1,book2)
 print(shelf)
-
 
